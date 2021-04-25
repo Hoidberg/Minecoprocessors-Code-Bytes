1aff2827fd5c31089318bc325906b1c6e326773b
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2021-04-25 09:53:44 -0500

    Create TripleANDGate.asm

---
+++ TripleANDGate.asm
@@ -0,0 +1,18 @@
+mov ports, 1110b
+start:
+mov a, pl
+mov b, pr
+mov c, pb
+add a, b
+add a, c
+cmp a, 0xFD
+jmp compare
+true:
+mov pf, 0xFF
+jmp start
+false:
+mov pf, 0x00
+jmp start
+compare:
+jz true
+jmp false
